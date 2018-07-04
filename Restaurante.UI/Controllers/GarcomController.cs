7b3e69c3d7b11f4ed2a217228d6ffb88473ab05e
Uirá Peixoto de Oliveira <uira.oliveira@compline.inf.br>
2018-07-04 13:28:33 -0300

    arrumando o layout

--- Restaurante.UI/Controllers/GarcomController.cs
+++ Restaurante.UI/Controllers/GarcomController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurante.UI.ActionFilters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,7 @@ using System.Web.Mvc;
 
 namespace Restaurante.UI.Controllers
 {
+    [IncludeLayoutData]
     public class GarcomController : Controller
     {
         // GET: Garcom
