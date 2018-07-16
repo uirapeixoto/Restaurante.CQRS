3c615f31c195ed7700e5b413df602042c8639fbb
pc-note\Desenvolvedor <uira.peixoto@gmail.com>
2018-07-16 08:32:47 -0300

    implementando o marcar como servido

--- Restaurante.Command/Mesas/Command/PedidoItemCommand.cs
+++ Restaurante.Command/Mesas/Command/PedidoItemCommand.cs
@@ -9,6 +9,10 @@ namespace Restaurante.Command.Mesas.Command
         public string Descricao { get; }
         public int Quantidade { get; }
         public decimal Ajuste { get; }
+        public DateTime? AServir { get; }
+        public DateTime? EmPreparacao { get; }
+        public DateTime? Servido { get; }
+
         public DateTime DataServico {
             get
             {
@@ -16,11 +20,14 @@ namespace Restaurante.Command.Mesas.Command
             }
         }
 
-        public PedidoItemCommand(int menuId, string descricao, int quantidade, decimal ajuste = (decimal) 0.0)
+        public PedidoItemCommand(int menuId, string descricao, int quantidade, DateTime? aServir, DateTime? emPreparacao, DateTime? servido, decimal ajuste = (decimal) 0.0)
         {
             MenuId = menuId;
             Descricao = descricao;
             Quantidade = quantidade;
+            AServir = aServir;
+            EmPreparacao = emPreparacao;
+            Servido = servido;
             Ajuste = ajuste;
         }
     }
