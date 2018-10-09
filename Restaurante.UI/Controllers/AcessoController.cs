67bf45d3576fbd9a7feaf35de464257a806c6c90
uira.peixoto <uira.peixoto@gmail.com>
2018-10-09 17:59:43 -0300

    criando tela de login

--- Restaurante.UI/Controllers/AcessoController.cs
+++ Restaurante.UI/Controllers/AcessoController.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Restaurante.UI.ViewModel;
+using System;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,9 +10,56 @@ namespace Restaurante.UI.Controllers
     public class AcessoController : Controller
     {
         // GET: Acesso
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
         {
+            var model = new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            //Não faça isso em produção
+            if(model.Login == "uirapeixoto" && model.Senha == "123" )
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "William"),
+                    new Claim(ClaimTypes.Email, "williambuzatto@gmail.com"),
+                    new Claim(ClaimTypes.Country, "Brasil"),
+                },
+                "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetrRedirectUrl(model.ReturnUrl));
+            }
+
+            //Autenticação falhou
+            ModelState.AddModelError("", "usuário ou senha inválidos");
             return View();
+        }
+
+        private string GetrRedirectUrl(string returnUrl)
+        {
+            if(string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
         }
     }
 }
---
+++ Restaurante.UI/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Restaurante.UI.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [HiddenInput]
+        public string ReturnUrl { get; set; }
+    }
+}
